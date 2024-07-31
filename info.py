566c2ae42adab6733d4fa477f0602f8b78ff1d76
MasalaMovie <168056962+MasalaMovie@users.noreply.github.com>
2024-07-31 13:43:30 +0545

    Update info.py

--- info.py
+++ info.py
@@ -79,6 +79,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
 
 LANGUAGES = ["malayalam", "mal", "tamil", "tam" ,"english", "eng", "hindi", "hin", "telugu", "tel", "kannada", "kan"]
 
